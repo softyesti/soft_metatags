1bb7aa304ca5ad473efe8cdea3a9390aa5f338a3
João Sereia <joao.lo.sereia@gmail.com>
2026-01-28 00:58:15 -0300

    feat(rust/soft_metatags): add function to read metadata from an audio file by path

--- rust/src/api/soft_metatags.rs
+++ rust/src/api/soft_metatags.rs
@@ -1,10 +1,23 @@
+use crate::api::entities::{meta::Meta, song::Song};
 use flutter_rust_bridge::frb;
+use lofty::{
+  file::{AudioFile, TaggedFileExt},
+  read_from_path,
+};
 
 #[frb(init)]
 pub fn init_app() {
-    flutter_rust_bridge::setup_default_user_utils();
+  flutter_rust_bridge::setup_default_user_utils();
 }
 
-pub fn minimal_adder(a: i32, b: i32) -> i32 {
-    a + b
+pub fn read(path: &String) -> Option<Song> {
+  let file = read_from_path(path).ok()?;
+
+  let tag = file.primary_tag();
+  let duration = file.properties().duration();
+
+  match tag {
+    None => Some(Song::new(path, duration, None)),
+    Some(tag) => Some(Song::new(path, duration, Some(Meta::from_tag(&tag)))),
+  }
 }
