72b05554e283e58cfc3e2ca02c5f87de833aed11
João Sereia <joao.lo.sereia@gmail.com>
2026-02-16 21:12:11 -0300

    chore(fvm): bump flutter version

--- hook/build.dart
+++ hook/build.dart
@@ -3,7 +3,7 @@ import 'package:native_toolchain_rust/native_toolchain_rust.dart';
 
 Future<void> main(List<String> args) async => build(
   args,
-  (BuildInput input, BuildOutputBuilder output) async {
+  (input, output) async {
     final builder = RustBuilder(
       cratePath: 'rust/',
       assetName: '${input.packageName}.dart',
